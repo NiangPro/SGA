d0b7e8cab899e674e9fcdac845712a814eff9256
NiangPro <NiangProgrammeur@gmail.com>
2021-11-12 13:20:05 +0000

    traitement connexion

--- src/com/lampfallDev/Main.java
+++ src/com/lampfallDev/Main.java
@@ -5,6 +5,8 @@
 package com.lampfallDev;
 
 import dataBase.Connexion;
+import forms.AdminDashboard;
+import forms.ClientDashboard;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -15,14 +17,15 @@ import javax.swing.JOptionPane;
  *
  * @author HP
  */
-public class Login extends javax.swing.JFrame {
-    private Connexion Db;
+public class Main extends javax.swing.JFrame {
+    private final Connexion Db;
     /**
      * Creates new form Login
      * @throws java.lang.ClassNotFoundException
      */
-    public Login() throws ClassNotFoundException {
+    public Main() throws ClassNotFoundException {
         initComponents();
+        this.setResizable(false);
         this.Db = new Connexion();
     }
 
@@ -49,37 +52,16 @@ public class Login extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(0, 102, 204));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\HP\\OneDrive\\Images\\concept-atm-homme-debout-pres-guichet-automatique_163786-655-removebg-preview.png")); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(37, 104, 209, 285));
 
         jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("GUICHET AUTOMATIQUE");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(37, 37, 37)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel2)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(37, 37, 37)
-                .addComponent(jLabel2)
-                .addGap(38, 38, 38)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 37, -1, -1));
 
         jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
         jLabel3.setText("CONNEXION");
@@ -111,7 +93,7 @@ public class Login extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(51, 51, 51)
                 .addComponent(jLabel3)
-                .addContainerGap(82, Short.MAX_VALUE))
+                .addContainerGap(92, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -173,22 +155,29 @@ public class Login extends javax.swing.JFrame {
         ResultSet res;
             var id = user_code.getText();
             var pass = user_nip.getText();
-            JOptionPane.showMessageDialog(null, pass);
-//            if(id.isEmpty()){
-//                JOptionPane.showMessageDialog(null, "Veuillez remplir tous les champs");
-//            }else{
-//                res = this.Db.login(id, pass);
-//            try {
-//                if(res.next()){
-//                    JOptionPane.showMessageDialog(null, "Code ou Nip incorrect");
-//                }else{
-//                    JOptionPane.showMessageDialog(null, "Connexion avec succes");  
-//                }
-//            } catch (SQLException ex) {
-//                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//                JOptionPane.showMessageDialog(null, this.Db.clients());
-//            }
+            if(id.isEmpty()|| pass.isEmpty()){
+                JOptionPane.showMessageDialog(null, "Veuillez remplir tous les champs");
+            }else{
+                res = this.Db.login(id, pass);
+                try {
+                    if(res.next()){
+                        if(res.getInt("estAdmin")== 1){
+                            JOptionPane.showMessageDialog(null, "Bienvenu Mr Administrateur ");
+                            new AdminDashboard().setVisible(true);
+                        }else{
+                            JOptionPane.showMessageDialog(null, "Bienvenu Mr Client ");
+                            new ClientDashboard().setVisible(true);
+                        }
+                        
+                        this.setVisible(false);
+//                        new Main().setVisible(true);                        
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Code ou Nip incorrect");  
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
             
 // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -210,22 +199,23 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
             try {
-                new Login().setVisible(true);
+                new Main().setVisible(true);
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
     }
